9720ff3249f722909fa0b7e13315c729b4dc96a7
Pwhitlock <pwhitlock131@gmail.com>
2024-02-16 17:14:39 -0600

    Completed get, post, put, and delete routes in product-routes.js and tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,59 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create({ tag_name: req.body.tag_name });
+    res.json(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    await Tag.update({ tag_name: req.body.tag_name }, { where: { id: req.params.id } });
+    const updatedTag = await Tag.findByPk(req.params.id);
+    res.json(updatedTag);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const qtyRemoved = await Tag.destroy({ where: { id: req.params.id } });
+    res.json(`${qtyRemoved} tag was removed from the databse`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
